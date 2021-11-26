5e1185844f2ed005b20dd00617413cf9f13e42a2
Acgnu <1814837440@qq.com>
2021-11-26 18:34:41 +0800

    file

---
+++ AcgnuX/Source/Bussiness/Constants/ReadSyncFileTaskProgressType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace AcgnuX.Source.Bussiness.Constants
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum ReadSyncFileTaskProgressType : byte
+    {
+        //仅显示文本提示
+        TEXT_ONLY = 1,
+        //文件读取完成
+        FILE_READED = 2
+    }
+}
