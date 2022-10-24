4ceb5536f7ae9c8c3376ee804aeab139e726a9b3
Acgnu <starkt921@gmail.com>
2022-10-24 23:24:09 +0800

    temp

--- AcgnuX/Source/ViewModel/MainWindowViewModel.cs
+++ AcgnuX/Source/ViewModel/MainWindowViewModel.cs
@@ -26,10 +26,17 @@ namespace AcgnuX.Source.ViewModel
     /// </summary>
     public class MainWindowViewModel : CommonWindowViewModel
     {
+        public int TitleHeightGridLength { get; set; } = 38;
         //菜单集合
         public ObservableCollection<NavMenu> navMenus { get; set; } = null;
-        public ICommand OnNavMenuItemClickCommand { get ; set; }
-        public Object mainContent {get; set;}
+        //菜单点击命令
+        public ICommand OnNavMenuItemClickCommand { get; set; }
+        //设置页面
+        public ICommand OnSettingCommand { get; set; }
+        //设置页面
+        private AppSettings appSettingsPage;
+        //主内容
+        private Object mainContent;
         public Object MainContent
         {
             get { return mainContent; }
@@ -54,18 +61,25 @@ namespace AcgnuX.Source.ViewModel
                 Environment.Exit(0);
             }));
             OnNavMenuItemClickCommand = new RelayCommand<NavMenu>(NavMenuItemClick);
+            OnSettingCommand = new RelayCommand<Window>(OnSettingIconClick);
         }
 
+        private void OnSettingIconClick(Window window)
+        {
+            if (null == appSettingsPage)
+            {
+                appSettingsPage = new AppSettings((MainWindow)window);
+            }
+            MainContent = appSettingsPage;
+        }
+
+        /// <summary>
+        /// 菜单点击命令实现
+        /// </summary>
+        /// <param name="navMenuItem"></param>
         private void NavMenuItemClick(NavMenu navMenuItem)
         {
             var item = navMenuItem;
-            //if (mainWindow.NavMenuListBox.SelectedIndex < 0)
-            //{
-            //    return;
-            //}
-            //切换Frame的Page内容
-            //this.ContentFrame.NavigationService.Navigate(new Uri(Convert.ToString(clickedBtn.Tag), UriKind.Relative));
-            //var item = navMenus[NavMenuListBox.SelectedIndex];
             if (null == item.instance)
             {
                 object[] parameters = new object[1];
@@ -73,7 +87,6 @@ namespace AcgnuX.Source.ViewModel
                 dynamic page = Activator.CreateInstance(item.pageType, parameters);
                 item.instance = page;
             }
-            //ContentFrame.Content = item.instance;
             MainContent = item.instance;
         }
 
@@ -85,15 +98,13 @@ namespace AcgnuX.Source.ViewModel
             navMenus = new ObservableCollection<NavMenu>()
             {
                 new NavMenu() { name = "疼逊云解析", pageType = typeof(DnsManage), icon=(Geometry)Application.Current.FindResource("Icon_Paperclip") },
-                new NavMenu() { name = "WEB服务",pageType = typeof (WebServer), icon=(Geometry)Application.Current.FindResource("Icon_Server") },
+                //new NavMenu() { name = "WEB服务",pageType = typeof (WebServer), icon=(Geometry)Application.Current.FindResource("Icon_Server") },
                 new NavMenu() { name = "密码库",pageType = typeof( PwdRepositroy), icon=(Geometry)Application.Current.FindResource("Icon_PasswordBox") },
                 new NavMenu() { name = "谱库",pageType = typeof( Tan8SheetReponsitory), icon=(Geometry)Application.Current.FindResource("Icon_Book") },
                 new NavMenu() { name = "爪机同步",pageType =typeof ( MobileDeviceControl), icon=(Geometry)Application.Current.FindResource("Icon_Cloud") },
                 new NavMenu() { name = "联系人",pageType =typeof ( ContactManage), icon=(Geometry)Application.Current.FindResource("Icon_ContactBoox") },
-                new NavMenu() { name = "设置",pageType = typeof (AppSettings), icon=(Geometry)Application.Current.FindResource("Icon_Setting") }
+                //new NavMenu() { name = "设置",pageType = typeof (AppSettings), icon=(Geometry)Application.Current.FindResource("Icon_Setting") }
             };
-
-            //DataContext = navMenus;
         }
     }
 }
